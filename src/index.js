9b182cb9c1ea4899939f512b262c8c72c4e8fcb6
lultik <mihwil27021@gmail.com>
2020-02-29 23:08:43 +0300

    feat: resolve

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-
 exports.min = function min (array) {
-    return array.length ? Math.min(array) : 0;
+    return (array && array.length !== 0) ? Math.min(...array) : 0;
 }
 
 exports.max = function max (array) {
- return array.length ? Math.max(array) : 0;
+ return (array && array.length !== 0) ? Math.max(...array) : 0;
 }
 
 exports.avg = function avg (array) {
-  return array.length ? array.reduce((acc, cur) => { return acc + cur}, 0)/array.length : 0
+  return (array && array.length !== 0) ? array.reduce((acc, cur) => { return acc + cur}, 0)/array.length : 0
 }
