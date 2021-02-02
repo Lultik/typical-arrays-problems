8abcf36c705edb790e3e2e24eee4278d083d2c7b
Lultik <mihwil27021@gmail.com>
2021-02-02 16:44:22 +0300

    feat: complete

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-exports.min = function min (array) {
-    return (array && array.length !== 0) ? Math.min(...array) : 0;
+exports.min = function min(array) {
+    return (array && array.length) ? Math.min(...array) : 0;
 }
 
-exports.max = function max (array) {
- return (array && array.length !== 0) ? Math.max(...array) : 0;
+exports.max = function max(array) {
+    return (array && array.length) ? Math.max(...array) : 0;
 }
 
-exports.avg = function avg (array) {
-  return (array && array.length !== 0) ? array.reduce((acc, cur) => { return acc + cur}, 0)/array.length : 0
+exports.avg = function avg(array) {
+    return (array && array.length) ? array.reduce((acc, cur) => {return acc + cur}, 0) / array.length : 0;
+
 }
